6bf456a3a9ed5664fcc0fcdd3c2a5e448e3dc54f
zanyPhi <phedas01@gmail.com>
2022-07-28 02:08:38 +0100

    Adding setup.cfg, license and README

--- setup.py
+++ setup.py
@@ -1,7 +1,10 @@
 from setuptools import setup
 
-setup(name='distributions',
+setup(name='bg_distributions',
       version='0.1',
-      description='Gaussian distributions',
-      packages=['distributions'],
+      author='Samuel Ojo',
+      author_email='phedas01@gmail.com',
+      description='Gaussian and Binomial distributions',
+      packages=['bg_distributions'],
+      install_requires=['math', 'matplotlib'],
       zip_safe=False)
