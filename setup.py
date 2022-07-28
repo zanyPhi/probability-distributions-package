bb636346eb3372e256617b16c09039b843e6a33a
zanyPhi <phedas01@gmail.com>
2022-07-28 22:21:25 +0100

    updated name and package name in setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,11 @@
 from setuptools import setup
 
-setup(name='bg_distributions',
+setup(name='continuous-and-discrete-distributions',
       version='0.1',
       author='Samuel Ojo',
       author_email='phedas01@gmail.com',
       description='Gaussian and Binomial distributions',
       url='https://github.com/zanyPhi/probability-distributions-package.git',
-      packages=['bg_distributions'],
+      packages=['continuous-and-discrete-distributions'],
       install_requires=['math', 'matplotlib'],
       zip_safe=False)
